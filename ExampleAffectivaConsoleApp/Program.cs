64c29e44648ce7e4957efcfd7ecda3caf065a68b
Christopher Kwan <ckwan@vistaprint.com>
2016-03-16 23:53:30 -0400

    Create initial Console Application that just says Hello World

---
+++ ExampleAffectivaConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExampleAffectivaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
