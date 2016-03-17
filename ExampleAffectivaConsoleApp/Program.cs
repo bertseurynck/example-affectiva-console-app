d2d57526ca9778f13fee18b5a6039d46f59dc21b
Christopher Kwan <ckwan@vistaprint.com>
2016-03-17 08:45:52 -0400

    Add license and classifier paths

--- ExampleAffectivaConsoleApp/Program.cs
+++ ExampleAffectivaConsoleApp/Program.cs
@@ -12,10 +12,18 @@ namespace ExampleAffectivaConsoleApp
         {
             Console.WriteLine("Example Affectiva Console App");
 
-            PhotoDetector detector = new PhotoDetector(1);
+            PhotoDetector detector = new PhotoDetector(1, FaceDetectorMode.LARGE_FACES);
 
-            String licensePath = @"";
+            String licensePath = @"D:\ExampleAffectivaConsoleApp\AffectivaLicense\sdk.license";
             detector.setLicensePath(licensePath);
+
+            String classifierPath=@"C:\Program Files\Affectiva\Affdex SDK\data";
+            detector.setClassifierPath(classifierPath);
+
+            detector.start();
+            //cwkTODO process
+            detector.stop();
+            detector.reset();
 
             // So the console window does not close immediately
             Console.ReadLine();
